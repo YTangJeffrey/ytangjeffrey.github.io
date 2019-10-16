30552fb6744071bc73b5b5b749aa33417fbcd21b
YTangJeffrey <36547398+YTangJeffrey@users.noreply.github.com>
2019-10-16 06:55:37 -0400

    backup
    backup

---
+++ Project2/sketch.js
@@ -0,0 +1,17 @@
+// function setup(){
+//    canvas =  createCanvas(windowWidth, windowHeight);
+//    canvas.position(0, 0);
+//    canvas.style('z-index','2');
+//    canvas.style('position','fixed');
+  
+//    //noCursor();
+// }
+
+// function draw(){
+//     ellipse(mouseX, mouseY, 100,100);
+//     fill(255,0);
+    
+//     strokeWeight(1);
+//     stroke(0,255);
+//     background(255,1);
+// }
